6cd022a1e6ed7539105ffb9fd4fb30643f459fd9
danhhh-anya <anyachan123321@gmail.com>
2021-02-08 13:15:32 +0200

    init commit

--- app/src/main/java/com/test/fragments/adapters/MyViewHolder.java
+++ app/src/main/java/com/test/fragments/adapters/MyViewHolder.java
@@ -13,18 +13,19 @@ import com.test.fragments.models.RecipeModel;
 public class MyViewHolder extends RecyclerView.ViewHolder {
     private TextView txtTitle;
     private TextView txtDescription;
-    private Button DeleteItem;
+    private Button btnDeleteItem;
 
     public MyViewHolder(@NonNull View itemView) {
         super(itemView);
         txtTitle = itemView.findViewById(R.id.txtTitle);
         txtDescription = itemView.findViewById(R.id.txtDescription);
+        btnDeleteItem = itemView.findViewById(R.id.btnDeleteItem);
     }
 
     public void onBind(RecipeModel recipeModel, MyRvAdapter.OnItemClick listener){
         txtTitle.setText(recipeModel.getTitle());
         txtDescription.setText(recipeModel.getDescription());
-        DeleteItem.setOnClickListener(v -> listener.onClick(recipeModel));
+        btnDeleteItem.setOnClickListener(v -> listener.onClick(recipeModel));
     }
 
 }
